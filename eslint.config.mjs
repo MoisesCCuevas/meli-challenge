f40f601ba088b59c305cd08699e96aaa429913d6
Moisés C. Cuevas <moisesccuevas@gmail.com>
2025-04-15 16:07:38 -0600

    fix build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/__test__/**/*.{ts,tsx}"],
+    rules: {
+      "react/no-children-prop": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
